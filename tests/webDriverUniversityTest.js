a2fc2dd18d7ec5d32da7ae91dce0fd14bb77a3b5
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-22 17:36:00 -0500

    Clase 25 Executing Mocha Test

--- tests/webDriverUniversityTest.js
+++ tests/webDriverUniversityTest.js
@@ -1,31 +1,25 @@
-describe("Verify whether  webdriveriouniversity link on homepage works correctly",
-	function(){
-		it("Check taht the contact us button opens the contact us page",
-			function(done){
-				return browser
-				.setViewportSize({
-					width:1200,
-					height: 700
-				})
-				.url('http://www.webdriveruniversity.com/')
-				.getTitle().then(function(title){
-					console.log('Title is: '+title);
-				})
-				.click("#contact-us")
-				.pause(3000);
-			});
-		it("Check taht the login button opens the Login page",
-			function(done){
-				return browser
-				.setViewportSize({
-					width:1200,
-					height: 700
-				})
-				.url('http://www.webdriveruniversity.com/')
-				.click('#login-portal')
-				.getTitle().then(function(title){
-					console.log('Title is: '+title);
-				})
-				.pause(3000);
-			});
+describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
+	it("check that the contact us button opens the contact us page", function(done) {
+		return browser
+		.setViewportSize({
+			width: 1200,
+			height: 800
+		})
+		.url('http://www.webdriveruniversity.com/')
+		.getTitle().then(function(title) {
+			console.log('Title is: ' + title);
+		})
+		.click("#contact-us")
+		.pause(1000)
 	});
+
+	it("check that the login button opens the login portal page", function(done) {
+		return browser
+		.url('http://www.webdriveruniversity.com/')
+		.click('#login-portal')
+		.getTitle().then(function(title) {
+			console.log('Title is: ' + title);
+		})
+		.pause(2000)
+	});
+});
