5fd3b69b607caa1cba49b45a4edd2d20b7183c67
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-28 09:59:18 -0500

    POM Phase 2 Part 3

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,20 +1,8 @@
-
-var request=require('sync-request');
-
+var ContactUs_Page= require("./pageObjects/ContactUs_Page.js");
 beforeEach(function(){
 	browser.url('/Contact-Us/contactus.html');
 });
 describe('Test Contact Us form WebdriverUni',function(){
-	var res=request('GET','http://jsonplaceholder.typicode.com/posts/1/comments');
-	var contactusDetails=JSON.parse(res.getBody().toString('utf8'));
-
-	var firstNameSelector="[name='first_name']";
-	var lastNameSelector="[name='last_name']";
-	var emailAddressSelector="[name='email']";
-	var commentsSelector="textarea";
-	var successfulSubmissionSelector="#contact_reply h1";
-	var unsuccessfulSubmissionSelector="body";
-	var submitButtonSelector="[type='submit']";
 
 	function setFirstName(first_name){
 		return browser.setValue(firstNameSelector,first_name);
