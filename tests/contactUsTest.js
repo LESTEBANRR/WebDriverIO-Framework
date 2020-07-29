a9d1fe3cb8594e9885be085c8f47cf41e11cd79d
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-29 13:02:18 -0500

    WDIO V5 P2

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -9,15 +9,6 @@ describe('Test Contact Us form WebdriverUni',function(){
 			ContactUs_Page.submitAllInformationViaContactUsForm('Joe','Blogs','joeBlogs@mail.com','How are you', true);
 	});
 
-	it('Test2: Should not be able to submit a successful submission via contact us form as all fields are required',
-		function(done){
-			ContactUs_Page.submitAllInformationViaContactUsForm('Mike','Woods','mike_woods@mail.com',null, false);
-	});
-
-	it('Test3: Should not be able to submit a successful submission via contact us form as all fields are required',
-		function(done){
-			ContactUs_Page.submitAllInformationViaContactUsForm('Sarah',null,'sarah_woods@mail.com',null, false);
-	});
 	it('Test4: Should not be able to submit a successful submission via contact us form as all fields are required',
 		function(done){
 			ContactUs_Page.submitAllInformationViaContactUsForm('Jin','Jones',null,null, false);
