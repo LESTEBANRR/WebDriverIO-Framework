11c0d5aadaa58e78a0cb13dd555bc6bffacf151a
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-29 10:40:36 -0500

    Allure Reports within Jenkins Part 4

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -5,20 +5,20 @@ beforeEach(function(){
 });
 describe('Test Contact Us form WebdriverUni',function(){
 
-	it('Should be able to submit a successful submission via contact us form',function(done){
+	it('Test1: Should be able to submit a successful submission via contact us form',function(done){
 			ContactUs_Page.submitAllInformationViaContactUsForm('Joe','Blogs','joeBlogs@mail.com','How are you', true);
 	});
 
-	it('Should not be able to submit a successful submission via contact us form as all fields are required',
+	it('Test2: Should not be able to submit a successful submission via contact us form as all fields are required',
 		function(done){
 			ContactUs_Page.submitAllInformationViaContactUsForm('Mike','Woods','mike_woods@mail.com',null, false);
 	});
 
-	it('Should not be able to submit a successful submission via contact us form as all fields are required',
+	it('Test3: Should not be able to submit a successful submission via contact us form as all fields are required',
 		function(done){
 			ContactUs_Page.submitAllInformationViaContactUsForm('Sarah',null,'sarah_woods@mail.com',null, false);
 	});
-	it('Should not be able to submit a successful submission via contact us form as all fields are required',
+	it('Test4: Should not be able to submit a successful submission via contact us form as all fields are required',
 		function(done){
 			ContactUs_Page.submitAllInformationViaContactUsForm('Jin','Jones',null,null, false);
 	});
