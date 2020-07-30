1b9f3940c7051b1b4dd86d8f4298919dc493d3b3
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-07-30 11:09:47 -0500

    Clase 142

---
+++ tests/homepageTest.js
@@ -0,0 +1,17 @@
+//const { expect } = require("chai");
+
+const { assert } = require("chai");
+
+describe("Test webdriveruni homepage",()=>{
+    it("Validate whether the webdriver uni homepage title is correct",()=>{
+        browser.url('./');
+        const title=browser.getTitle();
+        try{
+            expect(title).to.equal('WebDriverUniversity.com');
+        }catch(err){
+            console.log("Exception: "+err);
+            assert.fail();
+        }
+        
+    });
+});
